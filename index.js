1b680acca372a932691520d06c313e136ccdd0e2
samuelgarnham <1120118@my.brunel.ac.uk>
2015-10-25 17:16:27 -0400

    added index.js

--- index.js
+++ index.js
@@ -5,69 +5,85 @@ var xmlParser = require("xml2json"),
 var title = process.argv[2] || "carlsberg";
 var page = process.argv[3] || 1;
 
-var awsAccessKeyId = process.env.AWS_ACCESS_KEY_ID,
-	awsSecretKey = process.env.AWS_SECRET_KEY,
-	endpoint = "webservices.amazon.co.uk",
-	uri = "/onca/xml",
-	params = {
-	    "Service": "AWSECommerceService",
-	    "Operation": "ItemSearch",
-	    "AWSAccessKeyId": awsAccessKeyId,
-	    "AssociateTag": "httpsgithucom-21",
-	    "SearchIndex": "Grocery",
-	    "Keywords": "alcohol",
-	    "ResponseGroup": "Images,ItemAttributes",
-	    "Title": title,
-	    "Sort": "relevancerank",
-	    "ItemPage": page,
-	    "Timestamp": (new Date).toISOString()
-	},
+var urlConstructor = function(page, title) {
 
-	queryString = Object.keys(params).sort().reduce(function(arr, key) {
-		return arr.concat(encodeURIComponent(key) + "=" + encodeURIComponent(params[key]));
-	}, []).join("&"),
+	var awsAccessKeyId = process.env.AWS_ACCESS_KEY_ID,
+		awsSecretKey = process.env.AWS_SECRET_KEY,
+		endpoint = "webservices.amazon.co.uk",
+		uri = "/onca/xml",
+		params = [
+	            {"AWSAccessKeyId": awsAccessKeyId},
+	            {"AssociateTag": "httpsgithucom-21"},
+		    {"ItemPage": page},
+		    {"Keywords": "alcohol"},
+	            {"Operation": "ItemSearch"},
+		    {"ResponseGroup": "Images,ItemAttributes"},
+	            {"SearchIndex": "Grocery"},
+		    {"Service": "AWSECommerceService"},
+		    {"Sort": "relevancerank"},
+		    {"Timestamp": (new Date).toISOString()},
+		    {"Title": title}
+		],
+	
+		queryString = params.reduce(function(arr, pair) {
+			var key = Object.keys(pair);
+			return arr.concat(encodeURIComponent(key) + "=" + encodeURIComponent(pair[key]));
+		}, []).join("&");
+	
+		stringToSign = "GET\n" + endpoint + "\n" + uri + "\n" + queryString,
+		signature = crypto.createHmac("sha256", awsSecretKey).update(stringToSign).digest("base64");
 
-	stringToSign = "GET\n" + endpoint + "\n" + uri + "\n" + queryString,
-	signature = crypto.createHmac("sha256", awsSecretKey).update(stringToSign).digest("base64"),
-	url = "http://" + endpoint + uri + "?" + queryString + "&Signature=" + encodeURIComponent(signature);
+		return url = "http://" + endpoint + uri + "?" + queryString + "&Signature=" + encodeURIComponent(signature);
+};
 
+var amazonRequest = function (title, callback) {
+	
+	var allResults = []
+	var page = 1;
 
-var callback = function (error, response, body) {
-
-	if (!error && response.statusCode == 200) {
-		var parsed = JSON.parse(xmlParser.toJson(body, {sanitize: false})),
-			items = parsed.ItemSearchResponse.Items.Item;
-
-		var results = items.reduce(function(arr, item) {
-
-			var attrs = item.ItemAttributes,
-				title = attrs.Title,
-				feat = attrs.Feature,
-				alcohol = null,
-				i;
-
-			for (i in feat) {
-				var start = feat[i].search(/[0-9]*\.?[0-9]+%/);
-
-				if (start != -1) {
-					var end = feat[i].indexOf("%"),
-						alcohol = feat[i].slice(start, end); 
-
-					break;
+	var callback = function (error, response, body) {
+		
+		if (!error && response.statusCode == 200) {
+			var parsed = JSON.parse(xmlParser.toJson(body, {sanitize: false})),
+				items = parsed.ItemSearchResponse.Items.Item;
+	
+			var results = items.reduce(function(arr, item) {
+	
+				var attrs = item.ItemAttributes,
+					title = attrs.Title,
+					feat = attrs.Feature,
+					alcohol = null,
+					i;
+	
+				for (i in feat) {
+					var start = feat[i].search(/[0-9]*\.?[0-9]+%/);
+	
+					if (start != -1) {
+						var end = feat[i].indexOf("%"),
+							alcohol = feat[i].slice(start, end); 
+	
+						break;
+					};
+	
 				};
+	
+				return alcohol ? arr.concat({ "title": title, "alcohol": alcohol }) : arr;
+	
+			}, []);
+		
+			allResults.push(results);
+			request(url, callback);
+		};
 
-			};
+	};
 
-			return alcohol ? arr.concat({ "title": title, "alcohol": alcohol }) : arr;
-
-		}, []);
-
-
-		console.log(results);
+	while (true) {
+			
+	var url = urlConstructor(page, title);
+	request(url, callback);
+	
 	};
 
 };
-
-request(url, callback);
 
 
